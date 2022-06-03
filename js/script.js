73f4c278818e48da7f649b690e4bdd736fdf1ef8
vando-alves <vando.jesus29@gmail.com>
2022-06-02 22:09:31 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+
+const jump = () => {
+const mario = document.querySelector('.mario');
+
+mario.classList.add('jump');
+
+setTimeout(() =>
+
+mario.classList.remove('jump')
+
+,500)
+
+}
+
+
+const loop = setInterval(( )=>{
+
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80 ){
+
+    pipe.style.animation = 'none';
+    pipe.style.left =`${pipePosition}px`;
+    mario.style.animation = 'none';
+    mario.style.bottom =`${marioPosition}px`;
+    
+    mario.src="imagens/game-over.png";
+    mario.style.width = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop);
+}
+
+}
+,10)
+
+document.addEventListener('keydown', jump);
+
